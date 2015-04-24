9c64b14e713239db715007fa617d036a7fc49274
Alex Phelps <alexphelps@PRONTOLAP0028.local>
2015-04-24 13:27:23 +0700

    add material floating label

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 /*!
 * Scripts for Calltracker
 */
+
+//Floating Material Design Lables
+$('.form-control').on('focus blur', function (e) {
+    $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+}).trigger('blur');
